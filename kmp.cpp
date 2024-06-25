494098a5adf660154e2270d388b14910d25b5dae
DevaAgulla <87611913+DevaAgulla@users.noreply.github.com>
2024-06-25 12:08:55 +0530

    kmp algorithm use to find string matching

---
+++ kmp.cpp
@@ -0,0 +1,14 @@
+vector<int> kmp(string str){
+    int n = str.size();
+    cout << str << endl;
+    vector<int>lps(n,0);
+    for(int i=1;i<n;i++){
+        int pre = lps[i-1];
+        while(pre>0 and str[i]!=str[pre]){
+            pre = lps[pre-1];
+        }
+        
+        lps[i] = pre+(str[i]==str[pre]);
+    }
+    return lps;
+}
